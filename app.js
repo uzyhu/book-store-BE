927964017d5e85893031c53028b450b1144c6698
uzyhu <dnwlgn0807@naver.com>
2024-08-04 23:11:41 +0900

    connect book-store-fe

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 //express 모듈
-const express =  require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 //dot env 모듈
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
+app.use(
+  cors({
+    origin: "http://localhost:3001", // The frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.listen(process.env.PORT);
 
-const userRouter = require('./routes/users');
-const likeRouter = require('./routes/likes');
-const cartRouter = require('./routes/carts');
-const orderRouter = require('./routes/orders');
-const bookRouter = require('./routes/books');
-const categoryRouter = require('./routes/category');
+const userRouter = require("./routes/users");
+const likeRouter = require("./routes/likes");
+const cartRouter = require("./routes/carts");
+const orderRouter = require("./routes/orders");
+const bookRouter = require("./routes/books");
+const categoryRouter = require("./routes/category");
 
 app.use("/users", userRouter);
 app.use("/likes", likeRouter);
